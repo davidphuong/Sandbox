bfbe05923b5a06b9a2a1a8487f723af36f7d1b0a
David Phuong <davidphuong20@gmail.com>
2017-05-18 23:12:51 +0930

    Add dependency injection code

---
+++ DependencyInjection/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <boost/make_unique.hpp>
+
+enum class ShapeType { circle, triangle };
+
+class Shape {
+public:
+	virtual void draw() = 0;
+};
+
+class Circle : public Shape {
+public:
+	void draw() override {
+		std::cout << "Circle" << std::endl;
+	}
+};
+
+class Triangle : public Shape {
+public:
+	void draw() override {
+		std::cout << "Triangle" << std::endl;
+	}
+};
+
+class Drawing {
+public:
+	void draw_shape(std::unique_ptr<Shape> shape) {
+		m_shape = std::move(shape);
+		m_shape->draw();
+	}
+
+private:
+	std::unique_ptr<Shape> m_shape;
+};
+
+class IDrawing {
+public:
+	IDrawing() :
+		m_drawing(new Drawing)
+	{}
+
+	void draw(ShapeType type) {
+		switch (type) {
+		case ShapeType::circle:
+			m_drawing->draw_shape(boost::make_unique<Circle>());
+			break;
+		case ShapeType::triangle:
+			m_drawing->draw_shape(std::make_unique<Triangle>());
+			break;
+		default:
+			// Empty
+		}
+	}
+
+private:
+	Drawing* m_drawing{ nullptr };
+};
+
+int main(int argc, char* argv[]) {
+	IDrawing drawing;
+	drawing.draw(ShapeType::triangle);
+
+	system("pause");
+	return 0;
+}
